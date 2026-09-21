9dfdb109144250638dbb7b17ee3b30b2d79069f1
Rachmad Febriananda <rachmad17845@gmail.com>
2026-09-21 19:50:58 +0700

    praktikum 2, provider dan error handling

--- _04_week_4_networking_rest_api/lib/main.dart
+++ _04_week_4_networking_rest_api/lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'pages/post_list_page.dart';
+
+void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Week 4 - REST API',
-      theme: ThemeData(colorSchemeSeed: Colors.indigo, useMaterial3: true),
-      home: const Scaffold(body: Center(child: Text('Week 4 - Praktikum 1'))),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Week 4 - REST API',
+    theme: ThemeData(colorSchemeSeed: Colors.indigo, useMaterial3: true),
+    home: const PostListPage(),
+  );
 }
